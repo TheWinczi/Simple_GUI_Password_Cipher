a0fb1788d65dbe14ee6f45f598ab7c6c328f2334
TheWinczi <79306185+TheWinczi@users.noreply.github.com>
2021-05-27 12:21:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,32 +24,3 @@ def main():
 # Check is it main
 if __name__ == '__main__':
     main()
-
-""" Other methods
-
-    window = Tk()
-    
-    window.geometry("420x420")
-    window.title("This is title")
-
-    icon = PhotoImage(file = 'imgs/icon.png')
-    window.iconphoto(True, icon)
-    window.config(background = '#db6e00')
-
-    label = Label(window,
-                  text="Pierwszy label",
-                  fg = "red",
-                  bg = 'black',
-                  font=('Arial', 35, 'bold'),
-                  relief = RAISED,
-                  bd = 10,
-                  padx = 20,
-                  pady = 20,
-                  image = icon,
-                  compound = 'right')
-    label.pack()
-    # label.place(x=0, y=0)
-    
-    # main loop of window
-    window.mainloop()
-"""
